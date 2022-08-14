dfa3b0bad3638e800747535b3cd31cb1269a3d79
adityasingh321 <adityayarize@gmail.com>
2022-08-14 17:33:11 +0530

    styling for login and register completed

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,4 @@
-
-const gpwd_set = new Set()
+const gpwd_set = new Set();
 const passArray = [];
 
 // const noBucket = new Set()
@@ -7,83 +6,94 @@ const passArray = [];
 // const bucketB = new Set()
 // const bucketC = new Set()
 
-
-
 // let selectedBucket = document.getElementById();
 function onSelectImage(res) {
-    console.log(res);
-    let selectedBucket = document.querySelector('input[name="radios"]:checked').value;
-    // console.log(selectedBucket);
+  console.log(res);
+  let selectedBucket = document.querySelector('input[name="radios"]:checked').value;
+  // console.log(selectedBucket);
 
-    if(gpwd_set.has(res)){
-        if(res == passArray[passArray.length-1].split(':_:')[0]){
-            passArray.pop();
-            gpwd_set.delete(res);
-            document.getElementById(res).style.border = null;
-        }else{
-            console.log("Delete last element in order first!")
-        }
-    }else{
-        passArray.push(`${res}:_:${selectedBucket}`)
-        gpwd_set.add(res);
-        document.getElementById(res).style.border = "4px solid orange";
-        document.getElementById(res).style.borderRadius = '100%';
-        console.log(passArray)
+  if (gpwd_set.has(res)) {
+    if (res == passArray[passArray.length - 1].split(":_:")[0]) {
+      passArray.pop();
+      gpwd_set.delete(res);
+      document.getElementById(res).style.border = null;
+      document.getElementById(`${res}_num`).classList.remove('num');
+      document.getElementById(`${res}_num`).textContent = "";
+    } else {
+      console.log("Delete last element in order first!");
     }
-   
-    // if (gpwd_set.has(res)) {
-    //     console.log('true');
-    //     gpwd_set.delete(res);
-    //     document.getElementById(res).style.border = null;
-    // } else {
-    //     gpwd_set.add(res);
-    //     document.getElementById(res).style.border = "4px solid orange";
-    //     document.getElementById(res).style.borderRadius = '100%';
-    //     console.log(gpwd_set)
-    // }
+  } else {
+    passArray.push(`${res}:_:${selectedBucket}`);
+    gpwd_set.add(res);
+    document.getElementById(res).style.border = "4px solid orange";
+    document.getElementById(res).style.borderRadius = "100%";
+    document.getElementById(`${res}_num`).classList.add('num');
+    document.getElementById(`${res}_num`).textContent = passArray.length;
+    console.log(passArray);
+  }
+
+  // if (gpwd_set.has(res)) {
+  //     console.log('true');
+  //     gpwd_set.delete(res);
+  //     document.getElementById(res).style.border = null;
+  // } else {
+  //     gpwd_set.add(res);
+  //     document.getElementById(res).style.border = "4px solid orange";
+  //     document.getElementById(res).style.borderRadius = '100%';
+  //     console.log(gpwd_set)
+  // }
 }
 
-function post(path, params, method = 'post') {
+function post(path, params, method = "post") {
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  const form = document.getElementById("post-form");
+  form.method = method;
+  form.action = path;
 
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    const form = document.getElementById('post-form');
-    form.method = method;
-    form.action = path;
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement("input");
+      hiddenField.type = "hidden";
+      hiddenField.name = key;
+      hiddenField.value = params[key];
 
-    for (const key in params) {
-        if (params.hasOwnProperty(key)) {
-            const hiddenField = document.createElement('input');
-            hiddenField.type = 'hidden';
-            hiddenField.name = key;
-            hiddenField.value = params[key];
-
-            form.appendChild(hiddenField);
-        }
+      form.appendChild(hiddenField);
     }
+  }
 
-    document.body.appendChild(form);
-    form.submit();
+  document.body.appendChild(form);
+  form.submit();
 }
 
 // Submit post on submit
-var form = document.getElementById('post-form');
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let inputObj;
-    if(!(document.getElementById('email')) && !(document.getElementById('name'))){
-        inputObj = {
-                password: passArray
-            }
-    }else{
-        !(document.getElementById('name')) ? inputObj = {
-            email: document.getElementById('email').value,
-            password: passArray
-        } : inputObj = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            password: passArray
-        }
-    }
-    post('', inputObj)
+var form = document.getElementById("post-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let inputObj;
+  if (!document.getElementById("email") && !document.getElementById("name")) {
+    inputObj = {
+      password: passArray,
+    };
+  } else {
+    !document.getElementById("name")
+      ? (inputObj = {
+          email: document.getElementById("email").value,
+          password: passArray,
+        })
+      : (inputObj = {
+          name: document.getElementById("name").value,
+          email: document.getElementById("email").value,
+          password: passArray,
+        });
+  }
+  post("", inputObj);
 });
+
+function onVisibility() {
+  document.querySelector(".visibility").classList.toggle("hidden");
+  document.querySelector(".visibility1").classList.toggle("hidden");
+  document.querySelectorAll(".num").forEach(function (item) {
+    item.classList.toggle("hidden");
+  });
+}
